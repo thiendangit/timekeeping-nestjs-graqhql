2c770bf6774b1ff5c371a82f72a92fe4ed84dd5b
thiendangit <thiendangit1102@gmail.com>
2021-09-06 03:55:43 +0700

    add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      // The autoSchemaFile: true means that the schema will be generated in memory
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
